911d3a99d2ad75a43927d88b04149f7b529ac019
Robert-Mihail UNGUREANU <roungureanu@bitdefender.com>
2018-12-10 22:30:59 +0200

    Fixes and added junit lib

---
+++ Tema4/test/PerformanceTests.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+/**
+ *
+ * @author roungureanu
+ */
+public class PerformanceTests {
+    @Test
+    public void studentsExists() {
+        System.out.println("!!!!");
+        assertTrue(1 != 0);
+    }
+}
